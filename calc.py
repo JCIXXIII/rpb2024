27bbb28cde8870a3972e0d1bea5b4db54c673746
JCIXXIII <165990069+JCIXXIII@users.noreply.github.com>
2024-04-04 20:38:22 +0900

    calc.py

--- calc.py
+++ calc.py
@@ -4,11 +4,11 @@ def main():
     x = int(input("x > "))
     y = int(input("y > "))
     
-    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
-    
-    
-def add(a, b):
-    return a + b
+    print("%d / %d = %0.3f" % (x, y, add(x,y))
+
+          
+def add(a ,b):
+    return (a+b)        
 
 def divide(a, b):
     if b == 0:
@@ -16,6 +16,14 @@ def divide(a, b):
         #return ('Error: cannot divide by zero.')
     else:
         return(a/b)
+
+def multiply(a, b):
+    return (a*b)
+
+def subtract(a,b):
+    return (a-b)
+
+
 
 if __name__ == "__main__":
     main()
